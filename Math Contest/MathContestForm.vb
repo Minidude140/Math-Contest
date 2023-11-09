ff8401fe19c748548aed4fe7a3d775188d3aa2d6
Zac <zac.minidude140@Gmail.com>
2023-11-08 22:16:00 -0700

    moved age and grade range check into separate function.  Reversed user message to indicate not valid

--- Math Contest/MathContestForm.vb
+++ Math Contest/MathContestForm.vb
@@ -9,7 +9,7 @@
 '[~]only checks that anything is in the text boxes 
 '[~]age 7-11
 '[~]grade 1-4
-'[]only message user once
+'[~]only message user once
 '[]clear and focus offending text boxes
 
 Public Class MathContestForm
@@ -23,8 +23,6 @@ Public Class MathContestForm
     Private Function ValidateUserInputs(Optional ByRef message As String = "")
         Dim isvalid As Boolean = True
         Dim errorMessage As String
-        Dim age As Integer
-        Dim grade As Integer
 
         'Checks if each entered text box has a value
         For Each item As TextBox In StudentInfoGroupBox.Controls.OfType(Of TextBox)
@@ -34,32 +32,46 @@ Public Class MathContestForm
             If item.Text = "" Then
                 isvalid = False
                 errorMessage &= Replace($"{item.Name.ToString} is required{vbCrLf}", "TextBox", "")
-                MsgBox(errorMessage)
             End If
         Next
+        If AgeAndGradeCheck() = False Then
+            MsgBox("Student is not eligible to compete")
+        End If
+
+        MsgBox(errorMessage)
+        Return isvalid
+    End Function
+
+    Private Function AgeAndGradeCheck() As Boolean
+        Dim age As Integer
+        Dim grade As Integer
+        Dim validAgeAndGrade As Boolean = True
         Try
             'check if age or grade are numbers
             age = CInt(AgeTextBox.Text)
             grade = CInt(GradeTextBox.Text)
-
             'check if age is in range
             Select Case age
                 Case 7 To 11
                     'age is in range 
-                    MsgBox("Age is in range")
+                    'MsgBox("Age is in range")
+                Case Else
+                    validAgeAndGrade = False
             End Select
             'check if grade is in range
             Select Case grade
                 Case 1 To 4
                     'grade is in range
-                    MsgBox("Grade is in range")
+                    'MsgBox("Grade is in range")
+                Case Else
+                    validAgeAndGrade = False
             End Select
         Catch ex As Exception
-            isvalid = False
+            'validAgeAndGrade = False
             'not a number
             MsgBox("not a number")
         End Try
-        Return isvalid
+        Return validAgeAndGrade
     End Function
 
     'Event Handlers
